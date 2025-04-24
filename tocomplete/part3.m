c91cfe4203538227c0f00d013253e3fdd44e7468
cpap7 <mjajarmi2000@outlook.com>
2025-04-24 16:31:24 -0400

    Part 3 done now w/ proper documentation

--- tocomplete/part3.m
+++ tocomplete/part3.m
@@ -8,22 +8,24 @@ y = [1;0;1;0];
 
 %%% 3. Train a non-linear SVM classifier
 %%% PLACE YOUR CODE HERE
-svm_classifier = fitcsvm(X, y, 'KernelFunction', 'rbf', 'KernelScale', 'auto');
+svm = fitcsvm(X, y, ...             % Train SVM on X and y
+              'KernelFunction', 'rbf', ... % Use radial basis function (Gaussian) kernel
+              'KernelScale', 'auto');      % Let MATLAB choose the kernel scale automatically
+
 
 %%% 4. Compute decision regions
 %%% PLACE YOUR CODE HERE
 %%% Hint: Use plot_XOR_and_regions as inspiration
-decision = 0:0.01:1;
-[X1, X2] = meshgrid(decision, decision);
-XGrid = [X1(:), X2(:)];
-[~, scores] = predict(svm_classifier, XGrid);
-decision_values = scores(:, 2);
+d = 0:0.01:1;                      % Define a grid from 0 to 1 in steps of 0.01
+[X1, X2] = meshgrid(d, d);        % Create a full 2D grid of X1 and X2 values
+XGrid = [X1(:), X2(:)];           % Reshape the grid into a list of coordinates (Nx2)
+[~, scores] = predict(svm, XGrid);% Predict the class scores for the grid using the SVM
+decision_values = scores(:, 2);   % Extract the decision boundary values for the positive class
 
-figure; hold on;
-gscatter(X(:,1), X(:,2), y, 'rb', 'xo');
-contour(X1, X2, reshape(decision_values, size(X1)), [0,0], 'k');
-title('Non-linear SVM XOR Decision Boundary');
-legend('Class 1', 'Class 0', 'Decision Boundary');
+figure; hold on;                                 % Create a figure and enable multiple plots
+gscatter(X(:,1), X(:,2), y, 'rb', 'xo');          % Plot original XOR training points
+contour(X1, X2, reshape(decision_values, size(X1)), [0,0], 'k'); % Plot decision boundary
+title('Non-linear SVM XOR Decision Boundary');   % Add a title
+legend('Class 1', 'Class 0', 'Decision Boundary');% Add legend
 
-% might need to be changed a bit - wasn't sure if i formatted this
-% correctly
+% Should work now
