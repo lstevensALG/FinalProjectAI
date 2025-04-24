1d6dc799b30d7f91e8d8ffcd1043a0f01a7ec85d
cpap7 <mjajarmi2000@outlook.com>
2025-04-23 22:45:49 -0400

    part 3 implemented, might need to be revised based on the lab notes

--- tocomplete/part3.m
+++ tocomplete/part3.m
@@ -8,7 +8,19 @@ y = [1;0;1;0];
 
 %%% 3. Train a non-linear SVM classifier
 %%% PLACE YOUR CODE HERE
+svm_classifier = fitcsvm(X, y, 'KernelFunction', 'rbf', 'KernelScale', 'auto');
 
 %%% 4. Compute decision regions
 %%% PLACE YOUR CODE HERE
 %%% Hint: Use plot_XOR_and_regions as inspiration
+decision = 0:0.01:1;
+[X1, X2] = meshgrid(decision, decision);
+XGrid = [X1(:), X2(:)];
+[~, scores] = predict(svm_classifier, XGrid);
+decision_values = scores(:, 2);
+
+figure; hold on;
+gscatter(X(:,1), X(:,2), y, 'rb', 'xo');
+contour(X1, X2, reshape(decision_values, size(X1)), [0,0], 'k');
+title('Non-linear SVM XOR Decision Boundary');
+legend('Class 1', 'Class 0', 'Decision Boundary');
